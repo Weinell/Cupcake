7543b84a544d36a99b375d36d9839ce6e50eb491
Nikolaj <nikolajweinell@hotmail.com>
2021-10-15 04:53:46 +0200

    Changed how connection works

--- src/main/java/DBAccess/ConnectionConfig.java
+++ src/main/java/DBAccess/ConnectionConfig.java
@@ -6,20 +6,23 @@ public class ConnectionConfig {
 
     public static void main(String[] args) {
 
+        System.out.println("Loading driver...");
+
         try {
-
             Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://138.68.106.135:22/inventory","nikolaj","niko3460");
-
-            Statement statement = con.createStatement();
-            ResultSet rs = statement.executeQuery("select * from emp");
-            while (rs.next()) {
-                System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getInt(3));
-            }
-            con.close();
-        }catch (Exception e)    {
-            System.out.println(e);
+            System.out.println("Driver loaded!");
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("Cannot find the driver in the classpath!", e);
         }
-    }
 
+        System.out.println("Connecting to database...");
+
+        try (Connection con = DriverManager.getConnection("jdbc:mysql://138.68.106.135:22/inventory", "nikolaj", "niko3460");) {
+
+            System.out.println("Database connected");
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect to database", e);
+        }
+        
+    }
 }
